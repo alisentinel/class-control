a8f0f5a49bafbe562f630ff01209c2ca92c703e7
seyyedali <s.a.mirhosseini77@gmail.com>
2023-01-31 23:00:28 +0330

    add authotication to class-control

---
+++ app/Http/Controllers/sanctum/SanctumAuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\sanctum;
+
+use App\Http\Controllers\ApiController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class SanctumAuthController extends ApiController
+{
+    public function register(Request $request)
+    {
+        $validate = Validator::make($request->all(),[
+            'name' => 'required|string',
+            'username'=>'required|string|unique:users,username',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+        if ($validate->fails()) {
+            return $this->errorResponse('422',$validate->messages());
+        }
+        $user = User::query()->create([
+            'name' => $request->name,
+            'username'=>$request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+     
+        $token = $user->createToken('myApp')->plainTextToken;
+        return $this->successResponse(201,[
+            'user' => $user,
+            'token' => $token,
+        ],'user created successfully');
+    }
+
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(),[
+            //'username' => 'required|username|exists:users,username',
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required',
+        ]);
+        if ($validate->fails()) {
+            return $this->errorResponse('422',$validate->messages());
+        }
+        $user = User::query()->where('email',$request->email)->first();
+        if (!Hash::check($request->password,$user->password)) {
+            return $this->errorResponse('422','password is incorrect');
+        }
+        $token = $user->createToken('myApp')->plainTextToken;
+        return $this->successResponse(200,[
+            'user' => $user,
+            'token' => $token,
+        ],'user login ok');
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return $this->successResponse('200','logged out');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\sanctum\SanctumAuthController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\UniversityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +18,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::middleware('auth:sanctum')->group( function () {
 Route::resource('courses', CourseController::class);
 Route::resource('teachers', TeacherController::class);
 Route::resource('locations', LocationController::class);
 Route::resource('sessions', SessionController::class);
 Route::resource('university', UniversityController::class);
+});
+
+Route::post('register',[SanctumAuthController::class,'register']);
+Route::post('login',[SanctumAuthController::class,'login']);
+Route::get('logout',[SanctumAuthController::class,'logout'])->middleware('auth:sanctum');
